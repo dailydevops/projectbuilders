b864d1d6b5da45b37df61c097314bcf9ffc8a512
Martin Stühmer <me@samtrion.net>
2025-12-07 16:22:02 +0100

    test: Additional Tests for `TemporaryDirectoryBuilder`

---
+++ tests/NetEvolve.ProjectBuilders.Tests.Unit/Builders/TemporaryDirectoryBuilderTests.cs
@@ -0,0 +1,83 @@
+﻿namespace NetEvolve.ProjectBuilders.Tests.Integration.Builders;
+
+using System;
+using System.Threading.Tasks;
+using NetEvolve.ProjectBuilders.Builders;
+
+public class TemporaryDirectoryBuilderTests
+{
+    [Test]
+    public async Task CreateFile_WithFileName_ReturnsFile()
+    {
+        // Arrange
+        await using var directory = new TemporaryDirectoryBuilder();
+        var fileName = "file.txt";
+
+        // Act
+        var file = directory.CreateFile(fileName);
+
+        // Assert
+        using (Assert.Multiple())
+        {
+            _ = await Assert.That(file).IsNotNull();
+            _ = await Assert.That(file.CanWrite).IsTrue();
+        }
+    }
+
+    [Test]
+    public async Task CreateFile_WithExistingFileName_ThrowsArgumentException()
+    {
+        // Arrange
+        await using var directory = new TemporaryDirectoryBuilder();
+        var fileName = "file.txt";
+        _ = directory.CreateFile(fileName);
+
+        // Act
+        void Act() => directory.CreateFile(fileName);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>(Act);
+    }
+
+    [Test]
+    public async Task GetFilePath_WithFileName_ReturnsFilePath()
+    {
+        // Arrange
+        await using var tempDirectory = new TemporaryDirectoryBuilder();
+        var fileName = "file.txt";
+
+        // Act
+        var filePath = tempDirectory.GetFilePath(fileName);
+
+        // Assert
+        _ = await Assert.That(filePath).IsEqualTo(Path.Combine(tempDirectory.FullPath, fileName));
+    }
+
+    [Test]
+    public async Task GetFilePath_WithNullFileName_ThrowsArgumentNullException()
+    {
+        // Arrange
+        await using var tempDirectory = new TemporaryDirectoryBuilder();
+        string? fileName = null;
+
+        // Act
+        void Act() => tempDirectory.GetFilePath(fileName);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>(Act);
+    }
+
+    [Test]
+    public async Task GetFilePath_WithEmptyFileName_ThrowsArgumentException()
+    {
+        // Arrange
+        await using var tempDirectory = new TemporaryDirectoryBuilder();
+        var fileName = string.Empty;
+
+        // Act
+        void Act() => tempDirectory.GetFilePath(fileName);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>(Act);
+    }
+}
